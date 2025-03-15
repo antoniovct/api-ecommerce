453af85a31e164622a31d0011b1a21b3ef62ad0f
antoniovct <sousasoares9@gmail.com.com>
2025-03-15 00:05:34 -0300

    Início do projeto.

---
+++ src/test/java/com/antoniovictor/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniovictor.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
